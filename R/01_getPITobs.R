fb15ffe00ebd840d591a6d7e57aa27d869b6e0f0
Ryan Kinzer <ryank@nezperce.org>
2021-01-27 09:18:44 -0700

    added function to grab PITobs and started methods .Rmd

---
+++ R/01_getPITobs.R
@@ -0,0 +1,27 @@
+# Purpose: Download PIT-tag observations from DART
+# Author: Ryan N. Kinzer
+# 26 January 2021
+
+# load packages
+library(tidyverse)
+library(fisheR)
+
+# get data
+release_sites <- c('JOHTRP', 'SECTRP', 'IMNTRP', 'LOLOC', 'CLWRSF', 'LAKEC','SECESR')
+yrs <- 2000:2020
+
+cross2(release_sites, yrs) %>% map(set_names, c('x', 'y'))
+
+PIT_obs <- cross2(release_sites, yrs) %>%
+  map(set_names, c('site', 'year')) %>%
+  map_dfr(
+       .f = ~{
+         get_PITobs(
+           query_type = 'release_site',
+           release_site = .$site,
+           species = 'Steelhead',
+           run = 'All',
+           start_date = paste0('01/01/',.$year),
+           end_date = paste0('12/31/',.$year))
+       }
+  )
