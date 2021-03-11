864bfe18c61d17cf26add5cef80f1d65696165cd
Tyler Stright <tylers@nezpercetribe.org>
2021-03-11 14:47:35 -0700

    modified color schemes to match between plots. Still needs a bit of work but this sets us up nicely.

--- R/lgr_arrival_plot.R
+++ R/lgr_arrival_plot.R
@@ -11,6 +11,28 @@ transport_start <- ymd('2020-04-23') # begin barge collections
 spring_spill <- ymd('2020-04-03') # (https://www.fpc.org/WebForm2013/includes/Metadatafor2020VoluntarySpillWeb.pdf)
 summer_spill <- ymd('2020-06-21')
 
+# Colors for S_CHN and S_STH plots
+spsu_sites <- dart_my20 %>%
+  filter(run != 'Fall') %>%
+  distinct(release_site) %>%
+  pull(release_site)
+
+spsu_colors <- data.frame(
+  release_site = spsu_sites, 
+  plot_colors = viridis(n=length(spsu_sites))
+)
+
+# Colors for F_CHN plots
+fall_groups <- dart_my20 %>%
+  filter(run == 'Fall') %>%
+  distinct(release_groups) %>%
+  pull(unique(release_groups))
+
+fall_colors <- data.frame(
+  release_groups = fall_groups, 
+  plot_colors = viridis(n=length(fall_groups))
+)
+
 
 # Steelhead ----
 ggplot(dart_my20 %>% filter(species == 'Steelhead'), 
@@ -20,7 +42,10 @@ ggplot(dart_my20 %>% filter(species == 'Steelhead'),
   stat_ecdf(size=1) +
   facet_wrap(~plot_group, ncol=1) + #, strip.position='right') +
   scale_x_date(name='Date', labels = scales::date_format('%m/%d/%y'), date_breaks='month') +  
-  theme_bw() +  scale_color_viridis_d(direction=-1) +
+  theme_bw() +  
+  # scale_color_viridis_d(direction=-1) +
+  scale_color_manual(breaks= spsu_colors$release_site,
+                     values = spsu_colors$plot_colors) +
   theme(panel.grid.minor = element_blank()) +
   labs(y='Cumulative Proportion',
        color='Release Site', linetype='Operations')+
@@ -37,7 +62,10 @@ ggplot(dart_my20 %>% filter(species == 'Chinook salmon', run != 'Fall',
   stat_ecdf(size=1) +
   facet_wrap(~plot_group, ncol=1) +
   scale_x_date(name = 'Date', labels = scales::date_format('%m/%d/%y'), date_breaks='month') + 
-  theme_bw() +  scale_color_viridis_d(direction=-1) +
+  theme_bw() +  
+  # scale_color_viridis_d(direction=-1) +
+  scale_color_manual(breaks= spsu_colors$release_site,
+                     values = spsu_colors$plot_colors) +
   theme(panel.grid.minor = element_blank()) +
   labs(y = list(title='Cumulative Proportion'),
        color='Release Site', linetype='Operations') +
@@ -53,7 +81,10 @@ ggplot(dart_my20 %>% filter(species == 'Chinook salmon', run == 'Fall'),
   stat_ecdf(size=1) + 
   facet_wrap(~plot_group, ncol = 1) + 
   scale_x_date(name = 'Date', labels = scales::date_format('%m/%d/%y'), date_breaks='month') +
-  theme_bw() +   scale_color_viridis_d(direction=-1) +
+  theme_bw() +   
+  # scale_color_viridis_d(direction=-1) +
+  scale_color_manual(breaks= fall_colors$release_groups,
+                     values = fall_colors$plot_colors) +
   theme(panel.grid.minor = element_blank()) +
   labs(y = list(title='Cumulative Proportion'),
        color='Release Site', linetype='Operations') +
